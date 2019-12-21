175528e8d21e0207576ffa441e6dc2f2215f85d4
David Linus Briemann <ragingdave@mailbox.org>
2019-12-21 23:56:26 +0100

    this needs to continue

--- alphabeta.go
+++ alphabeta.go
@@ -1,26 +1,25 @@
 package chesskimo
 
-import "time"
+// // AlphaBeta runs a minmaxing search with alpha-beta pruning for the given parameters.
+// // It returns the best move for a full search of a given max depth. If a depth search has to
+// // be cancelled, the result of the next best depth is returned.
+// func AlphaBeta(engine *Engine, ss *SearchSettings, dostop *uint32) SearchResult {
+// 	startTime := time.Now()
+// 	maxTime := 5 * time.Second
+// 	sr := SearchResult{Move: BitMove(0)}
 
-// AlphaBeta runs a minmaxing search with alpha-beta pruning for the given parameters.
-// It returns the best move for a full search of a given max depth. If a depth search has to
-// be cancelled, the result of the next best depth is returned.
-func AlphaBeta(engine *Engine, ss *SearchSettings, dostop *uint32) SearchResult {
-	startTime := time.Now()
-	sr := SearchResult{Move: BitMove(0)}
+// 	sr.Depth = ss.MaxDepth
+// 	valid := alphaBeta(engine, ss.MaxDepth, -INFINITY, INFINITY, dostop, &startTime, maxTime, &sr)
+// 	if valid {
 
-	sr.Depth = ss.MaxDepth
-	valid := alphaBeta(engine, ss.MaxDepth, -INFINITY, INFINITY, dostop, &startTime, &sr)
-	if valid {
+// 	}
 
-	}
+// 	return sr
+// }
 
-	return sr
-}
+// func alphaBeta(engine *Engine, depth, alpha, beta int, dostop *uint32, startTime *time.Time, maxTime time.Duration, result *SearchResult) bool {
+// 	mlist := MoveList{}
+// 	board := engine.board
 
-func alphaBeta(engine *Engine, depth, alpha, beta int, dostop *uint32, startTime *time.Time, result *SearchResult) bool {
-	mlist := MoveList{}
-	board := engine.board
-
-	return true
-}
+// 	return true
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dbriemann/chesskimo
+
+go 1.13
